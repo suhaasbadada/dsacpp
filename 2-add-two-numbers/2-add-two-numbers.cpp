4717f6d5d18de5e66f73250a9352bc4912575605
Suhaas Rao Badada <suhaasbadada@gmail.com>
2022-03-10 09:54:33 +0530

    Time: 50 ms (54.19%), Space: 71.4 MB (50.05%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -17,28 +17,19 @@ public:
         ListNode* t2=l2;
         int carry=0;
         
-        
-        while(l1 || l2 || carry){
-            int s=0;
-            if(l1!=NULL){
-                s+=l1->val;
-                l1=l1->next;
-            }
-            if(l2!=NULL){
-                s+=l2->val;
-                l2=l2->next;
-            }
+        while(t1 || t2 || carry){
+            int cs=0;
+            if(t1){cs+=t1->val; t1=t1->next;}
+            if(t2){cs+=t2->val; t2=t2->next;}
             
-            s+=carry;      
-            carry=s/10;
-            // cout<<s<<endl;
+            cs+=carry;
+            carry=cs/10;
             
-            ListNode* curr_sum=new ListNode(s%10);
+            ListNode* curr_sum=new ListNode(cs%10);
             node->next=curr_sum;
             node=node->next;
+            
         }
-            
-            
         return rez->next;
     }
 };
