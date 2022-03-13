8cf346dbf3c4e5e15088d9ef0025cc65480211b0
Suhaas Rao Badada <suhaasbadada@gmail.com>
2022-03-13 13:36:52 +0530

    Time: 6 ms (11.70%), Space: 6.2 MB (80.05%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        char x;
+        int i;
+        for(i=0;i<s.length();i++){
+            if(s[i]=='('||s[i]=='{'||s[i]=='['){
+                st.push(s[i]);  
+            }
+                
+            if(st.empty()){
+                return false;
+            }
+
+            switch(s[i]){
+                case ')':
+                    x=st.top();
+                    st.pop();
+                    if(x=='{'||x=='[')
+                        return false;
+                    break;
+                    
+                case ']':
+                    x=st.top();
+                    st.pop();
+                    if(x=='{'||x=='(')
+                        return false;
+                    break;
+                    
+                case '}':
+                    x=st.top();
+                    st.pop();
+                    if(x=='('||x=='[')
+                        return false;
+                    break;
+            }
+                    
+        }
+        
+        return st.empty();
+    }
+};
